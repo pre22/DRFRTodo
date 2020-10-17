077ec47ac5c54b294562a1714b3212d74dfbf1b2
Precious Chika <43546663+pre22@users.noreply.github.com>
2020-10-17 13:57:34 +0100

    chore: updated views.py @ todos

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
+from todos.models import Todo
+from todos.serializers import TodoSerializer
 
-# Create your views here.
+
+# Displays all todos
+class ListTodo(generics.ListAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+# Displays a single model instance of each todo
+class DetailTodo(generics.RetrieveAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
